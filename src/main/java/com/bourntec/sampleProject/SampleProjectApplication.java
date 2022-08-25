8f089e4b40e46f868faa132d7b213982e7dd2a83
BharathReddyGopu <BharathReddyGopu@LAPTOP-8JMHIBP5>
2022-08-25 12:58:03 +0530

    initial changes

--- src/main/java/com/bourntec/sampleProject/SampleProjectApplication.java
+++ src/main/java/com/bourntec/sampleProject/SampleProjectApplication.java
@@ -3,6 +3,8 @@ package com.bourntec.sampleProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SampleProjectApplication {
 
